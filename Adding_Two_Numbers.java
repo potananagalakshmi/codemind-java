d5a156e395096d388fafe52d5444052519405805
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-05 11:33:46 +0530

    import java.util.Scanner; public class multiplication{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n1,n2,m;         n1=sc.nextInt();         n2=sc.nextInt();         m=n1+n2;         System.out.println(m);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class multiplication{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n1,n2,m;
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        m=n1+n2;
+        System.out.println(m);
+    }
+}
