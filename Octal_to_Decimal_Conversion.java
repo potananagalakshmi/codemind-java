6d042b56486393bce5de9871653658eb12357b7b
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-25 11:30:47 +0530

    import java.util.Scanner; public class conver{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String octal=sc.nextLine();         System.out.println(Integer.parseInt(octal,8));
        }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class conver{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String octal=sc.nextLine();
+        System.out.println(Integer.parseInt(octal,8));
+        
+        
+    }
+}
